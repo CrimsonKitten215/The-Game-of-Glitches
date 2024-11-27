7e83e20d35d23a6ecf53a449bf504cb3386b6915
Crimson Kitten <161948475+CrimsonKitten215@users.noreply.github.com>
2024-11-27 20:00:56 +0000

    Delete The Game of Glitches (Windows) directory

--- The Game of Glitches (Windows)/other/useful_code.py
+++
@@ -1,5 +0,0 @@
-def listify(string: str):
-    """
-    Converts a string into a list (the string must be split by commas)
-    """
-    return string.replace('"', "").replace("'", "").replace("[", "").replace("]", "").split(", ")
